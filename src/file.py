ff99ebbffb927871f7a537f0c8165b4c5389611d
mcecy <ceciliacadengue@gmail.com>
2022-09-05 04:07:29 -0300

    Alterando programa para separar todos os arquivos da pasta

--- src/file.py
+++ src/file.py
@@ -1,12 +1,15 @@
 import os
 from shutil import move
 
-def movefile(src_path, dest_path, file_type):
+def movefile(src_path, dest_path):
     for diretorio, subpastas, arquivos in os.walk(src_path):
-        for arquivo in arquivos:
-            if arquivo.endswith(f".{file_type}"):
-                if not os.path.exists(f"{dest_path}/{file_type}"):
-                    os.mkdir(f"{dest_path}/{file_type.upper()}")
-                    move(f"{src_path}/{arquivo}", f"{dest_path}/{file_type}/{arquivo}")
-                else:
-                    move(f"{src_path}/{arquivo}", f"{dest_path}/{file_type}/{arquivo}")
+        for diret in diretorio:
+            for subpasta in subpastas:
+                for arquivo in arquivos:
+                    *_, ext = os.path.splitext(f"{diret}/{subpasta}/{arquivo}")
+
+                    if not os.path.exists(f"{dest_path}/{ext}"):
+                        os.mkdir(f"{dest_path}/{ext.upper()}")
+                        move(f"{diret}/{subpasta}/{arquivo}", f"{dest_path}/{ext}/{arquivo}")
+                    else:
+                        move(f"{src_path}/{arquivo}", f"{dest_path}/{ext}/{arquivo}")
--- src/main.py
+++ src/main.py
@@ -2,31 +2,22 @@ import os
 from tkinter.filedialog import askdirectory
 from file import movefile
 
+#Ask user to select a source folder
+src_path = askdirectory(title = 'Select Folder')
+
 #CoDy's folder
-dest_path = "c:\\CoDy"
+dest_path = f"{src_path}\\CoDy"
 
 #Create destination folder if it doesn't exist
 exists = os.path.exists(dest_path)
 if not exists:
     os.mkdir(dest_path)
 
-#Ask user to select a source folder
-src_path = askdirectory(title = 'Select Folder')
+try:
+    print("Starting the tool...")
 
-while True:
-    file_type = input("Choose a file type to work with(pdf/mp4/rtf/docx/exe) or exit: ")
+    movefile(src_path, dest_path)
 
-    if file_type.lower() == 'exit':
-        quest = input("Want to choose another folder?(y/n) ")
-
-        if quest.lower() == 'n':
-            break
-        elif quest.lower() == 'y':
-            src_path = askdirectory(title = 'Select Folder')
-        else:
-            print("Invalid response.")
-    elif file_type.lower() == 'pdf' or 'mp4' or 'rtf' or 'docx' or 'exe':
-        movefile(src_path, dest_path, file_type)
-    else:
-        print("Invalid response.")
-
+    print("Finished.")
+except FileNotFoundError as e:
+    print(f"Error: {e}")
